b66d87b1d226b238b0c45c4fceddfe7e99118418
Andrei Damian <andreidaamian@gmail.com>
2022-05-23 22:57:47 +0300

    working with crypted connection

--- proxy_lib/src/lib.rs
+++ proxy_lib/src/lib.rs
@@ -34,32 +34,32 @@ struct Session {
 }
 
 impl Session {
-    fn write(&mut self, buf: &mut [u8]) {
-        if let Some(crypt) = &mut self.crypt {
-            crypt.enc.encrypt(buf);
+    fn new() -> Session {
+        Session {
+            read_buf: vec![],
+            write_buf: vec![],
+            crypt: None,
         }
+    }
+}
 
-        // for i in buf.iter() {
-        //     if i.is_ascii() && !i.is_ascii_control() {
-        //         print!("{}", *i as char);
-        //     }
-        // }
-
+impl Session {
+    fn write(&mut self, buf: &[u8]) {
+        let offset = self.write_buf.len();
         self.write_buf.extend_from_slice(buf);
+
+        if let Some(crypt) = &mut self.crypt {
+            crypt.enc.encrypt(&mut self.write_buf[offset..]);
+        }
     }
 
-    fn read(&mut self, buf: &mut [u8]) {
-        if let Some(crypt) = &mut self.crypt {
-            crypt.dec.decrypt(buf);
-        }
-
-        // for i in buf.iter() {
-        //     if i.is_ascii() && !i.is_ascii_control() {
-        //         print!("{}", *i as char);
-        //     }
-        // }
-
+    fn read(&mut self, buf: &[u8]) {
+        let offset = self.read_buf.len();
         self.read_buf.extend_from_slice(buf);
+
+        if let Some(crypt) = &mut self.crypt {
+            crypt.dec.decrypt(&mut self.read_buf[offset..]);
+        }
     }
 }
 
@@ -69,8 +69,6 @@ pub struct AuthData {
 }
 
 struct Proxy {
-    client: Session,
-    server: Session,
     state: ConnectionState,
     compression: bool,
     start_done: bool,
@@ -78,19 +76,15 @@ struct Proxy {
     tmp: Vec<u8>,
 }
 
+struct OnStartResult {
+    done: bool,
+    skip: bool,
+    count: usize,
+}
+
 impl Proxy {
     fn new(auth_data: AuthData) -> Proxy {
         Proxy {
-            client: Session {
-                read_buf: vec![],
-                write_buf: vec![],
-                crypt: None,
-            },
-            server: Session {
-                read_buf: vec![],
-                write_buf: vec![],
-                crypt: None,
-            },
             state: ConnectionState::Handshaking,
             compression: false,
             start_done: false,
@@ -137,157 +131,166 @@ impl Proxy {
         Ok(result)
     }
 
-    fn on_start(&mut self, direction: PacketDirection) -> Result<bool> {
+    fn on_start(
+        &mut self,
+        src_session: &mut Session,
+        direction: PacketDirection,
+    ) -> Result<OnStartResult> {
         self.tmp.clear();
-        match direction {
-            PacketDirection::ClientToServer => {
-                let session = &mut self.client;
-                let mut reader = Reader::new(&session.read_buf);
-                let p = protocol::deserialize_with_header(
-                    direction,
-                    self.state,
-                    &mut reader,
-                    self.compression,
-                    &mut self.tmp,
-                )?;
-                match p {
-                    Some((packet, size)) => {
-                        match packet {
-                            Packet::SetProtocolRequest(x) => {
-                                if x.next_state == 1 {
-                                    self.start_done = true;
-                                    self.state = ConnectionState::Status;
-                                } else if x.next_state == 2 {
-                                    self.state = ConnectionState::Login;
-                                } else {
-                                    unreachable!();
-                                }
-                            }
-                            _ => {}
+        let src = &src_session.read_buf;
+        let mut reader = Reader::new(src);
+        let p = protocol::deserialize_with_header(
+            direction,
+            self.state,
+            &mut reader,
+            self.compression,
+            &mut self.tmp,
+        )?;
+
+        let mut skip = false;
+        let mut count = 0;
+        let done = match p {
+            Some((packet, size)) => {
+                count = size;
+                println!("{:?}", packet);
+                match packet {
+                    Packet::SetProtocolRequest(x) => match x.next_state {
+                        1 => {
+                            self.start_done = true;
+                            self.state = ConnectionState::Status;
                         }
-                        session.read_buf.drain(..size);
+                        2 => {
+                            self.state = ConnectionState::Login;
+                        }
+                        _ => unimplemented!(),
+                    },
+                    // ---------------------------------------------------
+                    Packet::SuccessResponse(_) => {
+                        self.start_done = true;
+                        self.state = ConnectionState::Play;
                     }
-                    None => return Ok(false),
-                }
-            }
-            PacketDirection::ServerToClient => {
-                let session = &mut self.server;
-                let mut reader = Reader::new(&session.read_buf);
-                let p = protocol::deserialize_with_header(
-                    direction,
-                    self.state,
-                    &mut reader,
-                    self.compression,
-                    &mut self.tmp,
-                )?;
-                match p {
-                    Some((packet, size)) => {
-                        println!("{:?}", packet);
-                        match packet {
-                            Packet::SuccessResponse(_) => {
-                                self.start_done = true;
-                                self.state = ConnectionState::Play;
-                            }
-                            Packet::CompressResponse(x) => {
-                                self.compression = x.threshold >= 0;
-                            }
-                            Packet::EncryptionBeginResponse(packet) => {
-                                let aes_key: [u8; 16] = rand::random();
+                    Packet::CompressResponse(x) => {
+                        self.compression = x.threshold >= 0;
+                    }
+                    Packet::EncryptionBeginResponse(packet) => {
+                        skip = true;
+                        let aes_key: [u8; 16] = rand::random();
 
-                                let mut buffer = [0; 16];
-                                rand::thread_rng().fill_bytes(&mut buffer);
+                        let mut buffer = [0; 16];
+                        rand::thread_rng().fill_bytes(&mut buffer);
 
-                                let hash = {
-                                    let mut sha1 = sha1::Sha1::new();
-                                    sha1.update(packet.server_id);
-                                    sha1.update(aes_key);
-                                    sha1.update(&packet.public_key);
-                                    let hash = sha1.finalize();
+                        let hash = {
+                            let mut sha1 = sha1::Sha1::new();
+                            sha1.update(packet.server_id);
+                            sha1.update(aes_key);
+                            sha1.update(&packet.public_key);
+                            let hash = sha1.finalize();
 
-                                    num_bigint::BigInt::from_signed_bytes_be(&hash).to_str_radix(16)
-                                };
+                            num_bigint::BigInt::from_signed_bytes_be(&hash).to_str_radix(16)
+                        };
 
-                                let auth_data = self.auth_data.take().unwrap();
-                                let selected_profile = auth_data.selected_profile.replace("-", "");
+                        let auth_data = self.auth_data.take().unwrap();
+                        let selected_profile = auth_data.selected_profile.replace("-", "");
 
-                                #[allow(non_snake_case)]
-                                #[derive(Serialize)]
-                                struct RequestData {
-                                    accessToken: String,
-                                    selectedProfile: String,
-                                    serverId: String,
-                                }
-                                let req = RequestData {
-                                    accessToken: auth_data.access_token,
-                                    selectedProfile: selected_profile,
-                                    serverId: hash,
-                                };
-                                let req = serde_json::to_string(&req)?;
-                                let response = ureq::post(
-                                    "https://sessionserver.mojang.com/session/minecraft/join",
-                                )
+                        #[allow(non_snake_case)]
+                        #[derive(Serialize)]
+                        struct RequestData {
+                            accessToken: String,
+                            selectedProfile: String,
+                            serverId: String,
+                        }
+                        let req = RequestData {
+                            accessToken: auth_data.access_token,
+                            selectedProfile: selected_profile,
+                            serverId: hash,
+                        };
+                        let req = serde_json::to_string(&req)?;
+                        let response =
+                            ureq::post("https://sessionserver.mojang.com/session/minecraft/join")
                                 .set("Content-Type", "application/json; charset=utf-8")
                                 .send_string(&req)?;
 
-                                // 204 No Content = Ok
-                                if response.status() != 204 {
-                                    return Err(anyhow::Error::msg("bad mojang auth"));
-                                }
-
-                                let response = EncryptionBeginRequest {
-                                    shared_secret: &Proxy::rsa_crypt(&packet.public_key, &aes_key)?,
-                                    verify_token: &Proxy::rsa_crypt(
-                                        &packet.public_key,
-                                        &packet.verify_token,
-                                    )?,
-                                };
-                                let mut buf = Proxy::serialize_enc_response(response)?;
-                                self.server.write(&mut buf);
-
-                                let enc = Aes128Cfb8::new_from_slices(&aes_key, &aes_key).unwrap();
-                                let dec = Aes128Cfb8::new_from_slices(&aes_key, &aes_key).unwrap();
-
-                                self.server.crypt = Some(Encryption { enc, dec });
-                            }
-                            _ => {}
+                        // 204 No Content = Ok
+                        if response.status() != 204 {
+                            return Err(anyhow::Error::msg("bad mojang auth"));
                         }
-                        self.server.read_buf.drain(..size);
+
+                        let response = EncryptionBeginRequest {
+                            shared_secret: &Proxy::rsa_crypt(&packet.public_key, &aes_key)?,
+                            verify_token: &Proxy::rsa_crypt(
+                                &packet.public_key,
+                                &packet.verify_token,
+                            )?,
+                        };
+                        let mut buf = Proxy::serialize_enc_response(response)?;
+                        src_session.write(&mut buf);
+
+                        let enc = Aes128Cfb8::new_from_slices(&aes_key, &aes_key).unwrap();
+                        let dec = Aes128Cfb8::new_from_slices(&aes_key, &aes_key).unwrap();
+
+                        src_session.crypt = Some(Encryption { enc, dec });
                     }
-                    None => {
-                        return Ok(false);
-                    }
+                    _ => {}
                 }
+                false
             }
-        }
-        Ok(true)
+            None => true,
+        };
+        Ok(OnStartResult { done, skip, count })
     }
 
-    fn on_recv(&mut self, buf: &mut [u8], direction: PacketDirection) -> Result<()> {
-        match direction {
-            PacketDirection::ClientToServer => {
-                self.client.read(buf);
-                self.server.write(buf);
+    fn forward(
+        &mut self,
+        src: &mut Session,
+        dest: &mut Session,
+        buf: &[u8],
+        direction: PacketDirection,
+    ) -> Result<()> {
+        src.read(buf);
+        loop {
+            let result = self.on_start(src, direction)?;
+            if result.done {
+                break;
             }
-            PacketDirection::ServerToClient => {
-                self.server.read(buf);
-                self.client.write(buf);
+            if !result.skip {
+                dest.write(&src.read_buf[..result.count]);
             }
+            src.read_buf.drain(..result.count);
         }
-
-        while self.on_start(direction)? {}
 
         Ok(())
     }
+
+    fn on_recv(
+        &mut self,
+        buf: &[u8],
+        client: &mut Session,
+        server: &mut Session,
+        direction: PacketDirection,
+    ) -> Result<()> {
+        match direction {
+            PacketDirection::ClientToServer => self.forward(client, server, buf, direction),
+            PacketDirection::ServerToClient => self.forward(server, client, buf, direction),
+        }
+    }
+}
+
+struct RunStats {
+    read: usize,
+    write: usize,
 }
 
 fn run(
     mut client_socket: TcpStream,
     mut server_socket: TcpStream,
     auth_data: AuthData,
+    stats: &mut RunStats,
 ) -> Result<()> {
     const CLIENT_KEY: usize = 0;
     const SERVER_KEY: usize = 1;
 
+    let mut client = Session::new();
+    let mut server = Session::new();
     let mut proxy = Proxy::new(auth_data);
 
     let poller = Poller::new()?;
@@ -321,33 +324,36 @@ fn run(
                 if read == 0 {
                     return Ok(());
                 }
-                proxy.on_recv(&mut buffer[..read], direction)?;
+                stats.read += read;
+                proxy.on_recv(&buffer[..read], &mut client, &mut server, direction)?;
             }
             if ev.writable {
                 if ev.key == CLIENT_KEY {
-                    let wrote = client_socket.write(&proxy.client.write_buf)?;
-                    proxy.client.write_buf.drain(..wrote);
+                    let wrote = client_socket.write(&client.write_buf)?;
+                    stats.write += wrote;
+                    client.write_buf.drain(..wrote);
                 } else {
-                    let wrote = server_socket.write(&proxy.server.write_buf)?;
-                    proxy.server.write_buf.drain(..wrote);
+                    let wrote = server_socket.write(&server.write_buf)?;
+                    stats.write += wrote;
+                    server.write_buf.drain(..wrote);
                 }
             }
         }
 
         poller.modify(
             &client_socket,
-            polling::Event {
+            Event {
                 key: CLIENT_KEY,
                 readable: true,
-                writable: !proxy.client.write_buf.is_empty(),
+                writable: !client.write_buf.is_empty(),
             },
         )?;
         poller.modify(
             &server_socket,
-            polling::Event {
+            Event {
                 key: SERVER_KEY,
                 readable: true,
-                writable: !proxy.server.write_buf.is_empty(),
+                writable: !server.write_buf.is_empty(),
             },
         )?;
     }
@@ -370,6 +376,8 @@ pub fn do_things(
     let server = TcpStream::connect(server_address)?;
     println!("connected to {}", server_address);
 
-    println!("{:?}", run(client, server, auth_data));
+    let mut stats = RunStats { read: 0, write: 0 };
+    println!("{:?}", run(client, server, auth_data, &mut stats));
+    println!("total read: {}\ntotal write: {}", stats.read, stats.write);
     Ok(())
 }
